c02b9799fc155872bb402b948d61b62a797ff113
EstebanPineyrua <pineyruae@gmail.com>
2025-11-22 12:13:18 -0300

    Update stock.js

--- js/stock.js
+++ js/stock.js
@@ -1,20 +1,28 @@
 const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQwXhcFDzDXJ98LRtTUrVm7xenO1LxS2hFBWI9bZol9fJ-NP5yX5Gmighd76o5QkM1t-P_1oOdBTQuO/pub?gid=632464503&single=true&output=csv';
 
-    fetch(url)
-      .then(res => res.text())
-      .then(text => {
-        const rows = text.trim().split('\n');
-        const tbody = document.querySelector('#tablaStock tbody');
+fetch(url)
+  .then(res => res.text())
+  .then(text => {
+    const rows = text.trim().split('\n');
+    const tbody = document.querySelector('#tablaStock tbody');
 
-        rows.slice(1).forEach(row => {
-          const cols = row.split(',');
-          const tr = document.createElement('tr');
-          cols.forEach(col => {
-            const td = document.createElement('td');
-            td.textContent = col;
-            tr.appendChild(td);
-          });
-          tbody.appendChild(tr);
-        });
-      })
-      .catch(err => console.error('Error cargando CSV', err));
+    rows.slice(1).forEach(row => {
+      const cols = row.split(',');
+      const tr = document.createElement('tr');
+
+      cols.forEach((col, index) => {
+        const td = document.createElement('td');
+
+        if (col.startsWith("http")) {
+          td.innerHTML = <a href="${col}" target="_blank">Ver producto</a>;
+        } else {
+          td.textContent = col;
+        }
+
+        tr.appendChild(td);
+      });
+
+      tbody.appendChild(tr);
+    });
+  })
+  .catch(err => console.error('Error cargando CSV', err));
